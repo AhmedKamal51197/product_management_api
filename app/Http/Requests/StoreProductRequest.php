da692cf6c042e731a2f5616f7344481818234834
Ahmed Kamal <eng.ahmedkamal357@gmail.com>
2025-04-28 08:06:10 +0300

    create products api

---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\NotNumbersOnly;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=>['required', 'string', 'max:255',new NotNumbersOnly()],
+            'description'=>['required', 'string', 'max:255',new NotNumbersOnly()],
+            'price'=>['required', 'numeric', 'min:0'],
+            'quatnity'=>['required', 'integer', 'min:0'],
+            'image'=>['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+            'status'=>['required', 'boolean'],
+            
+        ];
+    }
+}
